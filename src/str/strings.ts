8fde381e2a925e34dc85b5a7e6b6ed6b987da7ec
theoriaarhidoxa <88836579+theoriaarhidoxa@users.noreply.github.com>
2022-11-23 14:36:28 +0300

    Add files via upload

---
+++ src/str/strings.ts
@@ -0,0 +1,6 @@
+export const stringData = {
+    ALL_KATAS: `https://www.codewars.com/api/v1/users/catchaser/code-challenges/completed?page=`,
+    SINGLE_KATA: `https://www.codewars.com/kata/`,
+    KATA_BY_ID: `https://www.codewars.com/api/v1/code-challenges/`,
+    BACKWARDS: `backwards`
+};
